643e69e08549eb9b00b6d9386ae8e3b492c7bed1
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-05-29 09:27:47 +0700

    D

--- index.js
+++ index.js
@@ -56,12 +56,50 @@ app.delete("/bank/:id", async(req, res) => {
   }
 })
 
+// CREATE ACCOUNT
+app.post("/account_create", async (req, res) => {
+  try {
+    const { account_number } = req.body;
+    const account = await db.Account.create({ account_number });
+    return res.status(201).json({ account });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
+// CREATE CUSTOMER
 
+app.post("/customer_create", async (req, res) => {
+  try {
+    const { customer_name, bank_id, account_id } = req.body;
+    const customer = await db.Customer.create({ customer_name, bank_id, account_id  });
+    return res.status(201).json({ customer });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 
-
-
-
+// BANK -> CUSTOMER -> ACCOUNT. 
+app.get("/bank", async (req, res) => {
+ try {
+   const banks = await db.Bank.findAll({
+     include: [
+       {
+         model: db.Customer,
+         include: [
+           {
+             model: db.Account
+           }
+         ]
+       },
+     ],
+   });
+   return res.status(200).json({ banks });
+ } catch (error) {
+  console.log(error);
+   return res.status(500).json({ error });
+ }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- models/customer.js
+++ models/customer.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Customer.belongsTo(models.Bank, { foreignKey: "bank_id" });
+      Customer.belongsTo(models.Account, { foreignKey: "account_id" });
     }
   }
   Customer.init({
